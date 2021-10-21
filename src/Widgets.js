7080872a438ccac3a53acd74463c0210c51e5c64
FurkanPerktas <90097407+FurkanPerktas@users.noreply.github.com>
2021-10-21 16:44:20 +0300

    changes

--- src/Widgets.js
+++ src/Widgets.js
@@ -22,12 +22,30 @@ function Widgets() {
         <InfoIcon />
       </div>
 
-      {newsArticle("PAPA React is back", "Top news - 9099 readers")}
-      {newsArticle("Coronavirus: UK updates", "Top news - 886 readers")}
-      {newsArticle("Tesla hits new highs", "Cars & Auto - 300 readers")}
-      {newsArticle("Bitcoin Breaks $22k", "Crypto - 8000 readers")}
-      {newsArticle("Is Redux too good?", "Code - 123 readers")}
-      {newsArticle("PAPA react launches course!", "Top news - 6503 readers")}
+      {newsArticle(
+        "React is back",
+        "Top news - 9099 readers"
+      )}
+      {newsArticle(
+        "Coronavirus: UK updates",
+        "Top news - 886 readers"
+      )}
+      {newsArticle(
+        "Tesla hits new highs",
+        "Cars & Auto - 300 readers"
+      )}
+      {newsArticle(
+        "Bitcoin Breaks $22k",
+        "Crypto - 8000 readers"
+      )}
+      {newsArticle(
+        "Is Redux too good?",
+        "Code - 123 readers"
+      )}
+      {newsArticle(
+        "launches course!",
+        "Top news - 6503 readers"
+      )}
     </div>
   );
 }
